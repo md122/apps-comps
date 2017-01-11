d96020c0752ae595704c93fd1bdfeeb25a9ab38a
Wanwanorange <yaow@carleton.edu>
2017-01-11 15:31:19 -0600

    add Level class

---
+++ AppsComps/Level.swift
@@ -0,0 +1,13 @@
+//
+//  Level.swift
+//  AppsComps
+//
+//  Created by appscomps on 1/11/17.
+//  Copyright © 2017 appscomps. All rights reserved.
+//
+
+import UIKit
+
+class Level: UIButton {
+    
+}
