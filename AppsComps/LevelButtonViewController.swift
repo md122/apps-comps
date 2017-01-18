a28b38f800b6374097db5d4034502964ea126dc0
md122 <dowm@carleton.edu>
2017-01-18 16:55:02 -0600

    Brynna adding Wanchen's changes

---
+++ AppsComps/LevelButtonViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LevelButtonViewController.swift
+//  AppsComps
+//
+//  Created by appscomps on 1/18/17.
+//  Copyright © 2017 appscomps. All rights reserved.
+//
+
+import UIKit
+
+class LevelButtonViewController: UICollectionView {
+    
+}
+
