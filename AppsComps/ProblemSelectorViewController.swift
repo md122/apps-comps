3d73b17777a536b120d281f702f9388d8ee37c1b
Sam Neubauer <samneub@gmail.com>
2017-01-26 13:10:32 -0600

    Functioning navigation

--- AppsComps/ProblemSelectorViewController.swift
+++ AppsComps/ProblemSelectorViewController.swift
@@ -38,22 +38,33 @@ class ProblemSelectorViewController: UIViewController {
         levelText.text = "You are on level " + String(level)
         
         
-        // Set the UIButton to Logout if root the mainviewcontroller
-        if let navController = self.navigationController, navController.viewControllers.count >= 2 {
-            let viewController = navController.viewControllers[navController.viewControllers.count - 2]
-            let lastViewController = viewController.nibName
+        // Set the UIButton to Logout if the less than 2 items on navigation stack
+        // This occurs when a going straight from the login to student view
+        // In other words make a logout button on student view, but not if coming from teacher
+        if let navController = self.navigationController, navController.viewControllers.count < 2 {
+            let leftButton: UIBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(self.logoutClicked(_:)))
+            leftButton.tintColor = .red
+            self.navigationItem.leftBarButtonItem = leftButton
         }
-        // (self, action: "buttonClicked:", for: .touchUpInside)
-        
-
     }
     
+    
+    // Note there is a similar logout in Teacher, changes to one should also go in the other
+    // At some point Sam should figure out how to just merge the two, because this is sloppy
     @IBAction func logoutClicked(_ sender: AnyObject) {
+        let createAccountAlert = UIAlertController(title: "Log out", message: "Are you sure you want to log out?", preferredStyle: UIAlertControllerStyle.alert)
+        
+        // Log out option
+        createAccountAlert.addAction(UIAlertAction(title: "Log Out", style: .destructive, handler: { (action: UIAlertAction!) in
         if (GIDSignIn.sharedInstance().hasAuthInKeychain()) {
             GIDSignIn.sharedInstance().signOut()
         }
-        
         currentUser = nil
         UIApplication.shared.keyWindow?.rootViewController?.dismiss(animated: true, completion: nil)
+        }))
+        // cancel option
+        createAccountAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action: UIAlertAction!) in
+        }))
+        present(createAccountAlert, animated: true, completion: nil)
     }
 }
