1b95daf615658ba6148c5d2cf01be313ff7f5ec8
Brynna Mering <brynnamering@eduroam-171-107.dyn.carleton.edu>
2017-02-21 04:07:24 -0600

    Fixed merge conflicts

--- AppsComps/ProblemSelectorReusableView.swift
+++ AppsComps/ProblemSelectorReusableView.swift
@@ -2,19 +2,32 @@
 //  ProblemSelectorReusableView.swift
 //  AppsComps
 //
-//  Created by appscomps on 2/19/17.
+//  Created by Brynna Mering on 2/21/17.
 //  Copyright © 2017 appscomps. All rights reserved.
 //
 
 import UIKit
 
 class ProblemSelectorReusableView: UICollectionReusableView {
+//    @IBOutlet weak var classroomText: UILabel!
+   
     @IBOutlet weak var classroomText: UILabel!
+    
+//    @IBOutlet weak var joinButton: UIButton!
     @IBOutlet weak var joinButton: UIButton!
+    
+//    @IBOutlet weak var leaveButton: UIButton!
     @IBOutlet weak var leaveButton: UIButton!
+    
+//    @IBOutlet weak var greetingLabel: UILabel!
     @IBOutlet weak var greetingLabel: UILabel!
+    
+//    @IBOutlet weak var levelLabel: UILabel!
     @IBOutlet weak var levelLabel: UILabel!
+    
+//    @IBOutlet weak var logoView: UIImageView!
     @IBOutlet weak var logoView: UIImageView!
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -23,5 +36,4 @@ class ProblemSelectorReusableView: UICollectionReusableView {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-
 }
