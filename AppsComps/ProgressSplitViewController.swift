280b9f048db0755d1f32dfb83b493c582470145d
Brynna Mering <brynnamering@eduroam-171-107.dyn.carleton.edu>
2017-02-20 22:43:52 -0600

    Saving local changes

--- AppsComps/ProgressSplitViewController.swift
+++ AppsComps/ProgressSplitViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 
 class ProgressSplitViewController: UISplitViewController {
 
-    @IBOutlet var helpButton: UIBarButtonItem!
-    
-    @IBOutlet var logoutButton: UIBarButtonItem!
+//    @IBOutlet var helpButton: UIBarButtonItem!
+//    
+//    @IBOutlet var logoutButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Student Progress"
-        
-        // Do any additional setup after loading the view.
+        let logoutButton: UIBarButtonItem = UIBarButtonItem(title: "Log Out", style: .plain, target: self, action: #selector(self.logoutClicked(_:)))
+        logoutButton.tintColor = .red
+        self.navigationItem.rightBarButtonItem = logoutButton
+        let helpButton: UIBarButtonItem = UIBarButtonItem(title: "Help", style: .plain, target: self, action: #selector(self.helpClicked(_:)))
+        self.navigationItem.leftBarButtonItem = helpButton
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,21 +29,11 @@ class ProgressSplitViewController: UISplitViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func helpClicked(_ sender: AnyObject) {
-        let popOverVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "helpPopUpID") as! HelpViewController
-        popOverVC.popoverPresentationController?.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0)
-        self.addChildViewController(popOverVC)
-        popOverVC.view.frame = self.view.frame
-        self.view.addSubview(popOverVC.view)
-        popOverVC.didMove(toParentViewController: self)
-    }
-    
-    
-    @IBAction func logoutClicked(_ sender: AnyObject) {
-        let logoutAlert = UIAlertController(title: "Log out", message: "Are you sure you want to log out?", preferredStyle: UIAlertControllerStyle.alert)
+    func logoutClicked(_ sender: UIBarButtonItem) {
+        let logOutAlert = UIAlertController(title: "", message: "Are you sure you want to log out?", preferredStyle: UIAlertControllerStyle.alert)
         
         // Log out option
-        logoutAlert.addAction(UIAlertAction(title: "Log Out", style: .destructive, handler: { (action: UIAlertAction!) in
+        logOutAlert.addAction(UIAlertAction(title: "Log Out", style: .destructive, handler: { (action: UIAlertAction!) in
             if (GIDSignIn.sharedInstance().hasAuthInKeychain()) {
                 GIDSignIn.sharedInstance().signOut()
             }
@@ -48,29 +41,19 @@ class ProgressSplitViewController: UISplitViewController {
             UIApplication.shared.keyWindow?.rootViewController?.dismiss(animated: true, completion: nil)
         }))
         // cancel option
-        logoutAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action: UIAlertAction!) in
+        logOutAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action: UIAlertAction!) in
         }))
-        present(logoutAlert, animated: true, completion: nil)
+        present(logOutAlert, animated: true, completion: nil)
     }
-
     
-//    @IBAction func logoutClicked(_ sender: AnyObject) {
-//        let logoutAlert = UIAlertController(title: "Log out", message: "Are you sure you want to log out?", preferredStyle: UIAlertControllerStyle.alert)
-//        
-//        // Log out option
-//        logoutAlert.addAction(UIAlertAction(title: "Log Out", style: .destructive, handler: { (action: UIAlertAction!) in
-//            if (GIDSignIn.sharedInstance().hasAuthInKeychain()) {
-//                GIDSignIn.sharedInstance().signOut()
-//            }
-//            currentUser = nil
-//            UIApplication.shared.keyWindow?.rootViewController?.dismiss(animated: true, completion: nil)
-//        }))
-//        // cancel option
-//        logoutAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action: UIAlertAction!) in
-//        }))
-//        present(logoutAlert, animated: true, completion: nil)
-//    }
-    
+    func helpClicked(_ sender: UIBarButtonItem) {
+        let popOverVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "helpPopUpID") as! HelpViewController
+        popOverVC.popoverPresentationController?.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0)
+        self.addChildViewController(popOverVC)
+        popOverVC.view.frame = self.view.frame
+        self.view.addSubview(popOverVC.view)
+        popOverVC.didMove(toParentViewController: self)
+    }
 
     /*
     // MARK: - Navigation
