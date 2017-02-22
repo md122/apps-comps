399b29b9b86f24edd5a9d1214a45e2e93e727ec9
Brynna Mering <brynnamering@eduroam-171-107.dyn.carleton.edu>
2017-02-21 20:18:56 -0600

    Fixed toolbar bug

--- AppsComps/ProgressSplitViewController.swift
+++ AppsComps/ProgressSplitViewController.swift
@@ -10,18 +10,19 @@ import UIKit
 
 class ProgressSplitViewController: UISplitViewController {
 
+    @IBOutlet var logoutButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Student Progress"
-        let logoutButton: UIBarButtonItem = UIBarButtonItem(title: "Log Out", style: .plain, target: self, action: #selector(self.logoutClicked(_:)))
+        //let logoutButton: UIBarButtonItem = UIBarButtonItem(title: "Log Out", style: .plain, target: self, action: #selector(self.logoutClicked(_:)))
         logoutButton.tintColor = .red
         //self.navigationItem.rightBarButtonItem = logoutButton
 //        let helpButton: UIBarButtonItem = UIBarButtonItem(title: "Help", style: .plain, target: self, action: #selector(self.helpClicked(_:)))
 //        self.navigationItem.leftBarButtonItem = helpButton
-//        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: self, action: nil)
-//        var toolbarItems = [logoutButton]
-//        self.navigationController?.setToolbarItems(toolbarItems, animated: false)
-//        self.navigationController?.setToolbarHidden(false, animated: false)
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: self, action: nil)
+        toolbarItems = [flexibleSpace, logoutButton]
+        self.navigationController?.setToolbarItems(toolbarItems, animated: false)
+        //self.navigationController?.setToolbarHidden(false, animated: false)
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,7 +30,7 @@ class ProgressSplitViewController: UISplitViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func logoutClicked(_ sender: UIBarButtonItem) {
+    @IBAction func logOutClicked(_ sender: AnyObject) {
         let logOutAlert = UIAlertController(title: "", message: "Are you sure you want to log out?", preferredStyle: UIAlertControllerStyle.alert)
         
         // Log out option
@@ -44,15 +45,34 @@ class ProgressSplitViewController: UISplitViewController {
         logOutAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action: UIAlertAction!) in
         }))
         present(logOutAlert, animated: true, completion: nil)
-    }
     
+    }
+//    func logoutClicked(_ sender: UIBarButtonItem) {
+//        let logOutAlert = UIAlertController(title: "", message: "Are you sure you want to log out?", preferredStyle: UIAlertControllerStyle.alert)
+//        
+//        // Log out option
+//        logOutAlert.addAction(UIAlertAction(title: "Log Out", style: .destructive, handler: { (action: UIAlertAction!) in
+//            if (GIDSignIn.sharedInstance().hasAuthInKeychain()) {
+//                GIDSignIn.sharedInstance().signOut()
+//            }
+//            currentUser = nil
+//            UIApplication.shared.keyWindow?.rootViewController?.dismiss(animated: true, completion: nil)
+//        }))
+//        // cancel option
+//        logOutAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action: UIAlertAction!) in
+//        }))
+//        present(logOutAlert, animated: true, completion: nil)
+//    }
+//    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using [segue destinationViewController].
         // Pass the selected object to the new view controller.
         if(segue.identifier == "FromTeacherToProblemSelector") {
-            splitViewController?.preferredDisplayMode = UISplitViewControllerDisplayMode.primaryHidden
-            splitViewController?.presentsWithGesture = true
+//            splitViewController?.preferredDisplayMode = UISplitViewControllerDisplayMode.primaryHidden
+//            splitViewController?.presentsWithGesture = true
+            self.preferredDisplayMode = UISplitViewControllerDisplayMode.primaryHidden
+            self.navigationController?.setToolbarHidden(true, animated: false)
         }
         
     }
