7bd95ee4db5e1b77367c79cee5d48c5c8c41fae4
Brynna Mering <brynnamering@eduroam-171-107.dyn.carleton.edu>
2017-02-22 17:49:24 -0600

    Fixed popover alignment

--- AppsComps/StudentCollectionViewController.swift
+++ AppsComps/StudentCollectionViewController.swift
@@ -184,9 +184,9 @@ class StudentCollectionViewController: UICollectionViewController, UICollectionV
             
             //controller?.delegate = self
             //you could set the following in your storyboard
-            controller?.sourceView = self.view
-            controller?.sourceRect = CGRect(x:self.view.bounds.midX, y: self.view.bounds.midY,width: 315,height: 230)
-            controller?.permittedArrowDirections = UIPopoverArrowDirection(rawValue: 0)
+//            controller?.sourceView = self.view
+//            controller?.sourceRect = CGRect(x:self.view.bounds.midX, y: self.view.bounds.midY,width: 315,height: 230)
+//            controller?.permittedArrowDirections = UIPopoverArrowDirection(rawValue: 0)
             
         }
         
