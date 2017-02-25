090924f086761682446c8baea0470bf1634eb049
Wanwanorange <yaow@carleton.edu>
2017-02-25 14:33:35 -0600

    Fixed help popup and deleted Problem Selector Class

---
+++ AppsComps/AddClassroomTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  AddClassroomTableViewCell.swift
+//  AppsComps
+//
+//  Created by Brynna Mering on 2/23/17.
+//  Copyright © 2017 appscomps. All rights reserved.
+//
+
+import UIKit
+
+class AddClassroomTableViewCell: UITableViewCell {
+
+    @IBOutlet var addButton: UIButton!
+    
+    @IBOutlet var addLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
